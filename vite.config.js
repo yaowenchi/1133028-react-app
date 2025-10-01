df082a57583d0beb51c0e0b6fb0c29578f40fa61
DESKTOP-F0GH6SG\USER <DESKTOP-F0GH6SG\USER>
2025-10-01 17:10:14 +0800

    feat: 菜單頁面

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),]
+  plugins: [react(), tailwindcss()], 
+  server:{
+    port:3000,
+    open:true,
+  }
 })
